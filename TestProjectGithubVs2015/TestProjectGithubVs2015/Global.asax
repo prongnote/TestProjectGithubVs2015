d0deb3f71263f71862a296147a3be59a1a51c765
prongnote <36097533+prongnote@users.noreply.github.com>
2018-02-03 15:09:12 +0700

    FirstBuild

---
+++ TestProjectGithubVs2015/TestProjectGithubVs2015/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TestProjectGithubVs2015.MvcApplication" Language="C#" %>
---
+++ TestProjectGithubVs2015/TestProjectGithubVs2015/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestProjectGithubVs2015.Startup))]
+namespace TestProjectGithubVs2015
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
